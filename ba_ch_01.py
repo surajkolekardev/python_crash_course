36ba316d9106b89d164555b873a39d789375754a
corporatejourney <suraj.kolekar083@gmail.com>
2024-10-01 08:45:17 +0530

    first python code

--- ba_ch_01.py
+++ ba_ch_01.py
@@ -2,7 +2,16 @@
 Getting started
 """
 
-print("Let's start from the scratch....Professional Comeback...!")
+"""
+1. In this chapter I have learned how to install a python's latest version in to windows/linux.
+2. We can run python code from terminal itself. --> exit terminal use ctrl + z and then press enter.
+3. Even we can execute for loops as well in the terminal.
 
-for item in range(10):
-    print(item)
+Note: If you encounter any error, Step away from your computer, take a short break, and then try again.
+
+"""
+
+
+#First hello world program 
+
+print("Hello python world!")
